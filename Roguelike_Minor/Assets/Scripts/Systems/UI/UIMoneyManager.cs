a48742e0447fc0fec51f6ab9cbb2cc7f3855fa79
Freek Pluim <freekpluim@gmail.com>
2023-10-06 14:09:44 +0200

    Hooked Up UI

---
+++ Roguelike_Minor/Assets/Scripts/Systems/UI/UIMoneyManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Game.Systems
+{
+    public class UIMoneyManager : MonoBehaviour
+    {
+        [SerializeField] UIManager uiManager;
+        [SerializeField] TextMeshProUGUI moneyNumber;
+
+        private void Start()
+        {
+            uiManager.agent.stats.onMoneyChanged += UpdateMoney;
+        }
+
+        void UpdateMoney(int newNumber)
+        {
+            moneyNumber.text = newNumber.ToString();
+        }
+    }
+}
