e69279a74ffbb3bca2ec30f686428044e0596600
Mitchell Hartwig <493584@student.saxion.nl>
2023-10-11 13:17:58 +0200

    CameraAnimation

--- Roguelike_Minor/Assets/Design/UIUX Assets/TestScripts/TriggerAnimationTest.cs
+++ Roguelike_Minor/Assets/Design/UIUX Assets/TestScripts/TriggerAnimationTest.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TriggerAnimationTest : MonoBehaviour
-{
-    public Animator animationCamera;
+{public Animator animationCamera;
+    
   public void ButtonPressed()
     {
         animationCamera.enabled = true;
