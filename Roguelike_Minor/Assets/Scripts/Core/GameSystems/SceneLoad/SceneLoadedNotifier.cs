b8dd61ad73859abc1f77a63e79edb4f34e88b414
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2023-10-12 15:39:09 +0200

    created event bus

---
+++ Roguelike_Minor/Assets/Scripts/Core/GameSystems/SceneLoad/SceneLoadedNotifier.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Game.Core.GameSystems {
+    public class SceneLoadedNotifier : MonoBehaviour
+    {
+        private void Awake()
+        {
+            SceneManager.sceneLoaded += InvokeSceneLoaded;
+        }
+
+        private void InvokeSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            EventBus<SceneLoadedEvent>.Invoke(new SceneLoadedEvent());
+        }
+    }
+}
