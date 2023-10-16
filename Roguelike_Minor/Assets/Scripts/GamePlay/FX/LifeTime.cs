89ee0c6ad9ff923b69faa2b82914163988521bd3
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2023-10-16 16:19:50 +0200

    added a temporary explosion effect for the T2 item

---
+++ Roguelike_Minor/Assets/Scripts/GamePlay/FX/LifeTime.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Game {
+    public class LifeTime : MonoBehaviour
+    {
+        [SerializeField] private bool destroy = true;
+        [SerializeField] private float lifeTime = 1f;
+
+        private void Start()
+        {
+            StartCoroutine(LifeTimeCo());
+        }
+
+        private IEnumerator LifeTimeCo()
+        {
+            yield return new WaitForSeconds(lifeTime);
+            //end object
+            if (destroy) { Destroy(gameObject); }
+            else { gameObject.SetActive(false); }
+        }
+    }
+}
