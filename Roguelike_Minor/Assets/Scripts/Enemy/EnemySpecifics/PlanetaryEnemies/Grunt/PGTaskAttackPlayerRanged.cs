cb39fab05e3d84eed481940874793c2ac25cc450
Freek Pluim <freekpluim@gmail.com>
2023-10-18 13:56:37 +0200

    Cleaned up Grunt, planetFlying has bug, speed stat

--- Roguelike_Minor/Assets/Scripts/Enemy/EnemySpecifics/PlanetaryEnemies/Grunt/PGTaskAttackPlayerRanged.cs
+++ Roguelike_Minor/Assets/Scripts/Enemy/EnemySpecifics/PlanetaryEnemies/Grunt/PGTaskAttackPlayerRanged.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using Game.Enemy.Core;
 using Game.Core;
+using UnityEngine.AI;
 
 namespace Game.Enemy {
     public class PGTaskAttackPlayerRanged : BT_Node
     {
-        Transform transform;
-        Agent enemyAgent;
-        LayerMask enemyLayerMask;
+        private Transform transform;
+        private Transform target;
+
+        private NavMeshAgent agent;
+        private Agent enemyAgent;
+
         private float distanceToTarget;
 
-        public PGTaskAttackPlayerRanged(Transform transform, Agent enemyAgent, LayerMask enemyLayerMask)
+        public PGTaskAttackPlayerRanged(Transform transform, Agent enemyAgent, NavMeshAgent agent)
         {
             this.transform = transform;
             this.enemyAgent = enemyAgent;
-            this.enemyLayerMask = enemyLayerMask;
+            this.agent = agent;
         }
 
         public override NodeState Evaluate()
         {
-            Transform target = (Transform)GetData("Target");
-            if (GetData("DistanceToTarget") != null)
-            {
-                distanceToTarget = (float)GetData("DistanceToTarget");
-            }
-
+            target = (Transform)GetData("Target");
+            if (GetData("DistanceToTarget") != null) distanceToTarget = (float)GetData("DistanceToTarget");
+            
             //If target null: Fail
             if (target == null)
             {
@@ -40,9 +41,16 @@ namespace Game.Enemy {
             //else Running
             else if(distanceToTarget <= PGTree.rangedAttackRange)
             {
-                Debug.Log("shooting");
+                //Stop moving
+                agent.velocity = Vector3.zero;
+
+                //Rotate to target
+                Vector3 targetPostition = new Vector3(target.position.x, transform.position.y, target.position.z);
+                transform.LookAt(targetPostition);
+
                 enemyAgent.abilities.secondary.TryUse();
                 state = NodeState.RUNNING;   
+
             }
 
             return state;
