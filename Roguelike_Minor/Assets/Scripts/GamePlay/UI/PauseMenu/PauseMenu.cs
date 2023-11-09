912342a4aaab8dd901f1c5e233f23aaf3ee552a5
Niels de Gruijl <niels07@outlook.com>
2023-11-09 14:54:27 +0100

    GAME > MAIN MENU > GAME SCENE LOADING BROKEN

--- Roguelike_Minor/Assets/Scripts/GamePlay/UI/PauseMenu/PauseMenu.cs
+++ Roguelike_Minor/Assets/Scripts/GamePlay/UI/PauseMenu/PauseMenu.cs
@@ -1,5 +1,7 @@
+using Game.Core.GameSystems;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace Game
@@ -8,14 +10,39 @@ namespace Game
     {
         [SerializeField] private GameObject menu;
 
+        public bool paused;
+
         public void ActivateMenu()
         {
             menu.SetActive(true);
+            paused = true;
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
         }
 
         public void DeactivateMenu()
         {
             menu.SetActive(false);
+            paused = false;
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        public void Settings()
+        {
+
+        }
+
+        public void ExitGame()
+        {
+            if(Application.isEditor)
+            {
+                EditorApplication.ExitPlaymode();
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
