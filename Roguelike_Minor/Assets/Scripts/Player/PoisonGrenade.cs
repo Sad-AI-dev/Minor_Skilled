0ae0f0a73056ab9b9fa84028dca5c4ed7d2fe4e4
Wouter <wouter2000ma@gmail.com>
2023-11-13 12:42:34 +0100

    SFX Added
    Grenade splash sound
    Grunt get hit sound

--- Roguelike_Minor/Assets/Scripts/Player/PoisonGrenade.cs
+++ Roguelike_Minor/Assets/Scripts/Player/PoisonGrenade.cs
@@ -9,8 +9,11 @@ namespace Game.Player
     public class PoisonGrenade : Projectile
     {
         [SerializeField] private GameObject poisonCloud;
+        [SerializeField] private AudioPlayer AP;
 
         [HideInInspector] public float gravity;
+        
+        
 
         protected override void UpdateMoveDir()
         {
@@ -22,6 +25,7 @@ namespace Game.Player
         {
             GameObject poison = Instantiate(poisonCloud, transform.position, Quaternion.identity);
             poison.GetComponent<DOTExplosion>().source = source;
+            AP.Play(0);
         }
     }
 }
---
+++ Roguelike_Minor/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,92 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIENCE = 278617630U;
+        static const AkUniqueID PLAY_DASH = 2211787386U;
+        static const AkUniqueID PLAY_ENEMY_HIT = 2045007312U;
+        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_GRENADE = 2405798708U;
+        static const AkUniqueID PLAY_GRENADE_SPLAT = 3537222909U;
+        static const AkUniqueID PLAY_GRENADE_THROW = 2492084817U;
+        static const AkUniqueID PLAY_HITMARKER = 1731259975U;
+        static const AkUniqueID PLAY_OBELISK_PASSIVE = 2990404959U;
+        static const AkUniqueID PLAY_PICKUP_ITEM = 3128913152U;
+        static const AkUniqueID PLAY_PRIMARY_FIRE = 3563057235U;
+        static const AkUniqueID PLAY_SPECIAL_FIRE = 1149417670U;
+        static const AkUniqueID PLAY_START_OBELISK = 3577284316U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID GRAVEL = 2185786256U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID OBELISK_PROGRESS = 1308554018U;
+        static const AkUniqueID RUNNING_SPEED = 2309143834U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID AUXILIARY_BUSSES = 3174030640U;
+        static const AkUniqueID ENEMY = 2299321487U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID PLAYER = 1069431850U;
+        static const AkUniqueID UI = 1551306167U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID DEFAULT_VERB = 3018232304U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
