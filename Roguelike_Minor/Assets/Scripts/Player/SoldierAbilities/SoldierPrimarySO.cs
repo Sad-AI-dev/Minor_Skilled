e6442c268127f71580b021aebd670ce732afeb09
Niels de Gruijl <niels07@outlook.com>
2023-11-13 13:44:58 +0100

    Primary fire converted to raycast

--- Roguelike_Minor/Assets/Scripts/Player/SoldierAbilities/SoldierPrimarySO.cs
+++ Roguelike_Minor/Assets/Scripts/Player/SoldierAbilities/SoldierPrimarySO.cs
@@ -55,7 +55,7 @@ namespace Game.Player.Soldier
         public override void Use(Ability source)
         {
             Camera cam = Camera.main;
-            UnityEngine.Vector3 target;
+            UnityEngine.Vector3 target = UnityEngine.Vector3.zero;
             UnityEngine.Vector3 bulletDir;
 
             PrimaryVars vars = source.vars as PrimaryVars;
@@ -63,17 +63,27 @@ namespace Game.Player.Soldier
             controller.StartSlowCoroutine(source.coolDown * 1.1f);
 
             RaycastHit hit;
+            RaycastHit _hit = new RaycastHit();
             if (Physics.Raycast(cam.ViewportPointToRay(new UnityEngine.Vector3(0.5f, 0.5f, 0)), out hit, 500, layermask))
-                target = hit.point;
+            {
+                if (Physics.Raycast(source.originPoint.position, hit.point - source.originPoint.position, out _hit, 500, layermask))
+                    target = _hit.point;
+            }
             else
-                target = cam.ViewportToWorldPoint(new UnityEngine.Vector3(0.5f, 0.5f, 1000));
+                return;
 
+            if (_hit.transform.TryGetComponent<Agent>(out Agent agent))
+            {
+                agent.health.Hurt(new HitEvent(source));
+            }
+
+            vars.lineRenderer.enabled = true;
             UnityEngine.Vector3[] positions = { source.originPoint.position, target};
             vars.lineRenderer.SetPositions(positions);
             vars.lineRenderer.widthMultiplier = 0.2f;
-            vars.lineOpacity = 255f;
+            vars.lineOpacity = 1;
 
-            source.agent.StartCoroutine(ReduceLineOpacity(vars));
+            source.agent.StartCoroutine(ReduceLineOpacity(vars, source));
 
             /*//add inaccuracy
             vars.inaccuracy += source.coolDown * spreadBuildupSpeed;
@@ -153,15 +163,20 @@ namespace Game.Player.Soldier
                 vars.inaccuracy = 0;
         }
 
-        private IEnumerator ReduceLineOpacity(PrimaryVars vars)
+        private IEnumerator ReduceLineOpacity(PrimaryVars vars, Ability source)
         {
-            while(vars.lineOpacity > 0)
-            {
-                yield return null;
-                Color color = new Color(158, 52, 235, vars.lineOpacity);
-                vars.lineRenderer.SetColors(color, color);
-                vars.lineOpacity -= 25.5f;
-            }
+            /*            while(vars.lineOpacity > 0)
+                        {
+                            yield return null;
+                            Debug.Log("cancer");
+                            Color color = new Color(158, 52, 235, vars.lineOpacity);
+                            vars.lineRenderer.startColor = color;
+                            vars.lineRenderer.endColor = color;
+                            vars.lineOpacity -= 0.1f;
+                        }*/
+
+            yield return new WaitForSeconds(source.coolDown/2);
+            vars.lineRenderer.enabled = false;
         }
     }
 }
