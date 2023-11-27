a603006e136aecee4e7e44fd58fde5826fda6a82
Niels de Gruijl <niels07@outlook.com>
2023-11-27 12:42:04 +0100

    Added new T1, T2 and T3 SO's to hopefully fix a bug, implemented item 13

--- Roguelike_Minor/Assets/Scripts/GamePlay/Items/T2/Item13SO.cs
+++ Roguelike_Minor/Assets/Scripts/GamePlay/Items/T2/Item13SO.cs
@@ -10,38 +10,43 @@ namespace Game
     {
         public class MirrorReflectionItems : Item.ItemVars
         {
-            public float reflectionDamage;
+            public float totalDamage;
+            public float reflectionDamageMultiplier;
         }
 
         [Header("Damage")]
-        [SerializeField] private float damage;
-        [SerializeField] private float bonusDamage;
+        [SerializeField] private float damageMultiplier;
+        [SerializeField] private float bonusDamageMultiplier;
 
         //Initialize vars
 
         public override void InitializeVars(Item item)
         {
-            item.vars = new MirrorReflectionItems { reflectionDamage = damage};
+            item.vars = new MirrorReflectionItems { reflectionDamageMultiplier = 0};
         }
 
         //Manage stacks
 
         public override void AddStack(Item item)
         {
-            (item.vars as MirrorReflectionItems).reflectionDamage += bonusDamage;
+            MirrorReflectionItems vars = item.vars as MirrorReflectionItems;
+            if (item.stacks == 1) { vars.reflectionDamageMultiplier += damageMultiplier; }
+            else { vars.reflectionDamageMultiplier += bonusDamageMultiplier; }
         }
 
         public override void RemoveStack(Item item)
         {
             MirrorReflectionItems vars = item.vars as MirrorReflectionItems;
-            if(item.stacks == 0) { vars.reflectionDamage -= damage; }
-            else { vars.reflectionDamage -= bonusDamage; }
+            if(item.stacks == 0) { vars.reflectionDamageMultiplier -= damageMultiplier; }
+            else { vars.reflectionDamageMultiplier -= bonusDamageMultiplier; }
         }
 
         //Process HitEvents
 
         public override void ProcessTakeDamage(ref HitEvent hitEvent, Item sourceItem)
         {
+            if (!hitEvent.hasAgentSource) return;
+            (sourceItem.vars as MirrorReflectionItems).totalDamage = hitEvent.GetTotalDamage();
             DamageSource(hitEvent, sourceItem);
         }
 
@@ -50,13 +55,14 @@ namespace Game
             MirrorReflectionItems vars = item.vars as MirrorReflectionItems;
 
             HitEvent hitSource = new HitEvent();
-            hitSource.baseDamage = vars.reflectionDamage;
+            hitSource.baseDamage = vars.totalDamage * vars.reflectionDamageMultiplier;
             hitEvent.source.health.Hurt(hitSource);
         }
 
         public override string GenerateLongDescription()
         {
-            return "fuck off";
+            return "When hit reflect the damage, " +
+                   $"dealing {damageMultiplier * 100}% (+{bonusDamageMultiplier * 100}% per stack) damage of damage taken.";
         }
     }
 }
