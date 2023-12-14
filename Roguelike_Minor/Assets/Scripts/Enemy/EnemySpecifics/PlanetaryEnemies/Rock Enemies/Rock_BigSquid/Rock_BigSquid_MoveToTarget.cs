3213f8cf36bb676d2e2198c5f1ab6f15d2090a55
Freek Pluim <freekpluim@gmail.com>
2023-12-14 16:32:29 +0100

    Fix rock_bigsquid

---
+++ Roguelike_Minor/Assets/Scripts/Enemy/EnemySpecifics/PlanetaryEnemies/Rock Enemies/Rock_BigSquid/Rock_BigSquid_MoveToTarget.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Game.Enemy.Core;
+using Game.Core;
+
+namespace Game.Enemy
+{
+    public class Rock_BigSquid_MoveToTarget : BT_Node
+    {
+        NavMeshAgent navAgent;
+
+        public Rock_BigSquid_MoveToTarget(NavMeshAgent navAgent)
+        {
+            this.navAgent = navAgent;
+        }
+
+        public override NodeState Evaluate()
+        {
+            state = NodeState.RUNNING;
+
+            if(target != (Transform)GetData("Target")) target = (Transform)GetData("Target");
+
+            navAgent.SetDestination(target.position);
+
+            return state;
+        }
+    }
+}
