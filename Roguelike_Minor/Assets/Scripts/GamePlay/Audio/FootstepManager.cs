cabc07897253657c9e07633adb8d816c3964d2b9
Arvid van den Hoogen <arvidvdhoogen@gmail.com>
2023-12-15 11:32:28 +0100

    Added support for footstep sounds on objects

--- Roguelike_Minor/Assets/Scripts/GamePlay/Audio/FootstepManager.cs
+++ Roguelike_Minor/Assets/Scripts/GamePlay/Audio/FootstepManager.cs
@@ -1,56 +1,61 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using AK.Wwise;
-using Unity.VisualScripting;
 using UnityEngine;
+using AK.Wwise;
+
+using RTPCValuePair = Game.SoundObjectSetSO.RTPCValuePair;
 
 namespace Game {
     public class FootstepManager : MonoBehaviour
     {
-
+        [Header("Sound Settings")]
+        [SerializeField] private RTPC[] parameters;
         [SerializeField] private SoundMaterialSetSO soundMaterialSet;
+        [SerializeField] private SoundObjectSetSO soundObjectSet;
 
+        [Header("Raycast Settings")]
         [SerializeField] private LayerMask floorlayer;
 
         private CharacterController controller;
-        private bool blendTerrainTiles;
 
         private void Start()
         {
             controller = GetComponent<CharacterController>();
-            StartCoroutine(CheckGround());
         }
 
-        private void Update()
+        public void CheckGround()
         {
-            //CheckGround();
-        }
-
-     
-
-        private IEnumerator CheckGround()
-        {
-            while (true)
-            {
-                if (controller.velocity != Vector3.zero && Physics.Raycast(
-                        transform.position - new Vector3(0, 0, 0),
-                        Vector3.down, out RaycastHit hit, 0.5f))
+            if (TryFindGround(out RaycastHit hit)) {
+                ResetVolumes();
+                //unmute correct sounds
+                if (hit.collider.TryGetComponent(out Terrain terrain))
                 {
-                    if (hit.collider.TryGetComponent(out Terrain terrain))
-                    {
-                        SetFootstepSFXTerrain(terrain, hit.point);
-                    }
-                    else 
-                    {
-                        // Debug.Log("Hit object");
-                    }
+                    SetFootstepSFXTerrain(terrain, hit.point);
                 }
-
-                yield return null;
+                else if (hit.collider.TryGetComponent(out MeshRenderer meshRenderer))
+                {
+                    SetFootstepSFXObject(meshRenderer);
+                }
             }
         }
+        private bool TryFindGround(out RaycastHit hit)
+        {
+            //raycast
+            bool result = Physics.Raycast(transform.position, Vector3.down, out RaycastHit hitResult, 0.5f);
+            hit = hitResult;
+            //return result
+            return result;
+        }
 
+        //=========== Reset Volumes ============
+        private void ResetVolumes()
+        {
+            //mute all sounds
+            Array.ForEach(parameters, (RTPC param) => param.SetGlobalValue(0f));
+        }
+
+        //============ Terrain sound fx =============
         private void SetFootstepSFXTerrain(Terrain terrain, Vector3 hitpoint)
         {
             Vector3 TerrainPos = hitpoint - terrain.transform.position;
@@ -72,10 +77,12 @@ namespace Game {
             Dictionary<RTPC, float> percentagePerLayerType = new Dictionary<RTPC, float>();
             foreach (var kvp in layersAtPosition)
             {
-                RTPC param = soundMaterialSet.GetSwitchByTerrain(terrain.terrainData.terrainLayers[kvp.Key]);
+                RTPC param = soundMaterialSet.GetParamByTerrain(terrain.terrainData.terrainLayers[kvp.Key]);
                 if (!percentagePerLayerType.ContainsKey(param))
                 {
-                    percentagePerLayerType.Add(param, kvp.Value);
+                    if (kvp.Value > 0f) { //don't register 0% layers
+                        percentagePerLayerType.Add(param, kvp.Value);
+                    }
                 }
                 else
                 {
@@ -89,10 +96,12 @@ namespace Game {
              }
         }
 
-        private IEnumerator SetFootstepSFXRender(GameObject obj)
+        //================ GameObject Sounds ==================
+        private void SetFootstepSFXObject(MeshRenderer meshRenderer)
         {
-            return null;
+            RTPCValuePair[] parameters = soundObjectSet.GetRTPCValuePairFromMaterial(meshRenderer.material);
+            //set volumes based on parameters
+            Array.ForEach(parameters, (RTPCValuePair valuePair) => valuePair.parameter.SetGlobalValue(valuePair.value));
         }
-
     }
 }
--- Roguelike_Minor/Assets/Scripts/GamePlay/Audio/SFXFootstep.cs
+++ Roguelike_Minor/Assets/Scripts/GamePlay/Audio/SFXFootstep.cs
@@ -4,6 +4,10 @@ namespace Game
 {
     public class SFXFootstep : MonoBehaviour
     {
+        [Header("Refs")]
+        [SerializeField] private FootstepManager footstepManager;
+
+        [Header("Sound Settings")]
         [SerializeField] private AK.Wwise.Event footstepSFX;
         [SerializeField] private AK.Wwise.Event servoSFX;
 
@@ -11,11 +15,14 @@ namespace Game
         {
             if (animationEvent.animatorClipInfo.weight > 0.5f)
             {
+                //check ground
+                footstepManager.CheckGround();
+                //play sound
                 footstepSFX.Post(gameObject);
             }
         }
         
-         void playServo(AnimationEvent animationEvent)
+        void playServo(AnimationEvent animationEvent)
         {
             if (animationEvent.animatorClipInfo.weight > 0.5f)
             {
